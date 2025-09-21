aa57ac7b3d6651342decd36fe024720d2d40a6ae
Michael Luciuk <michael.luciuk@gmail.com>
2025-09-21 15:53:22 -0400

    Adjusting styling on pricing page, mobile

---
+++ static/assets/js/mobile-swiper.js
@@ -0,0 +1,24 @@
+
+function initSwiper(containerClass, paginationClass) {
+    return new Swiper(containerClass, {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        pagination: {
+            el: paginationClass,
+            clickable: true,
+        },
+        breakpoints: {
+            769: {
+                enabled: false,
+            },
+        },
+    });
+}
+
+const welcomeSwiper = initSwiper('.welcome-carousel', '.welcome-pagination');
+const costSwiper = initSwiper('.cost-carousel', '.cost-pagination');
+const remoteConfSwiper = initSwiper('.remote-conferencing-carousel', '.remote-conferencing-pagination');
+const aiConferencingSwiper = initSwiper('.ai-conferencing-carousel', '.ai-conferencing-pagination');
+const applicationSwiper = initSwiper('.application-carousel', '.application-pagination');
+const pricingSwiper = initSwiper('.pricing-carousel', '.pricing-pagination');
+const founderSwiper = initSwiper('.founder-carousel', '.founder-pagination');
