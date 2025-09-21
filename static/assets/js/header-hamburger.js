97ce46f214eb2e1efaaffd5219366978fa41d90a
Michael Luciuk <michael.luciuk@gmail.com>
2025-09-21 13:50:50 -0400

    Adjusting mobile nav styling

--- static/assets/js/header-hamburger.js
+++ static/assets/js/header-hamburger.js
@@ -4,3 +4,12 @@ const mobileNav = document.getElementById("mobile-nav");
 hamburger.addEventListener("click", () => {
     mobileNav.classList.toggle("active");
 });
+
+// Mobile dropdown toggle
+document.querySelectorAll('#mobile-nav .dropdown-toggle').forEach(toggle => {
+    toggle.addEventListener('click', e => {
+        e.preventDefault();
+        const parentLi = toggle.parentElement;
+        parentLi.classList.toggle('active');
+    });
+});
