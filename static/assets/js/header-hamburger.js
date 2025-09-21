9a6b66646526fb1c9f252f3650602bf4e2d08c45
Michael Luciuk <michael.luciuk@gmail.com>
2025-09-21 13:38:20 -0400

    Adding hamburger menu

--- static/assets/js/header-hamburger.js
+++ static/assets/js/header-hamburger.js
@@ -2,5 +2,5 @@ const hamburger = document.getElementById("hamburger");
 const mobileNav = document.getElementById("mobile-nav");
 
 hamburger.addEventListener("click", () => {
-    mobileNav.style.display = mobileNav.style.display === "block" ? "none" : "block";
+    mobileNav.classList.toggle("active");
 });
